440b20ecd4ed5fe3a11ab9e4ae53690c34864444
Yusuke Doi <yuusukedoi23@gmail.com>
2024-08-25 17:59:24 +0900

    リポジトリと使い方のリンクを表示する

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Board from './components/Board.tsx'
+import { FaGithub, FaQuestionCircle } from "react-icons/fa";
 
 
 function App() {
   return (
     <>
-      <h1>詰将棋盤</h1>
+      <div className='title-container'>
+        <h1>詰将棋盤</h1>
+        <div className='navigation'>
+          <a href='https://github.com/wai-doi/tsumeshogi-ban/'><FaGithub /></a>
+          <a href='https://github.com/wai-doi/tsumeshogi-ban/blob/main/README.md'><FaQuestionCircle /></a>
+        </div>
+      </div>
       <Board />
     </>
   )
