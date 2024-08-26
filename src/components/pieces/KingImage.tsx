7d833ef6bdb42d19aa613babefacce614f6ae69d
Yusuke Doi <yuusukedoi23@gmail.com>
2024-08-26 09:14:42 +0900

    Change king image

--- src/components/pieces/KingImage.tsx
+++ src/components/pieces/KingImage.tsx
@@ -1,5 +1,5 @@
-import Image from '../../images/black_king.png'
-import OppositeImage from '../../images/white_king.png'
+import Image from '../../images/black_king2.png'
+import OppositeImage from '../../images/white_king2.png'
 
 const Images = {
   normal: Image,
